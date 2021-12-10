2196a320253d5e448836a2aa56a4fbcb7af718c5
Nikita <nikita.c04@mail.ru>
2021-12-10 22:24:47 +0300

    final

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import time
 if __name__ == '__main__':
-    K = int(input('distance between stops: '))
-    N = int(input('distance traveled by SvetOchka: '))
+    distance_between_stops = int(input('distance between stops: '))
+    distance_traveled_by_SvetOchka = int(input('distance traveled by SvetOchka: '))
     time_start = time.time()
-    next_stop_in = N % K
-    previous_stop_in = K - next_stop_in
+    next_stop_in = distance_traveled_by_SvetOchka % distance_between_stops
+    previous_stop_in = distance_between_stops - next_stop_in
     nearest_stop_in = min(next_stop_in, previous_stop_in)
     print('distance to nearest stop:', nearest_stop_in)
     print('execution time:', time.time()-time_start)
