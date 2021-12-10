2d0a06ba85d4380dcf87d8d25fe8cb0542734071
Nikita <nikita.c04@mail.ru>
2021-12-10 18:02:11 +0300

    problem A done

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-bdfd
+import time
+if __name__ == '__main__':
+    K = int(input('distance between stops: '))
+    N = int(input('distance traveled by SvetOchka: '))
+    time_start = time.time()
+    next_stop_in = N % K
+    previous_stop_in = K - next_stop_in
+    nearest_stop_in = min(next_stop_in, previous_stop_in)
+    print('distance to nearest stop:', nearest_stop_in)
+    print('execution time:', time.time()-time_start)
